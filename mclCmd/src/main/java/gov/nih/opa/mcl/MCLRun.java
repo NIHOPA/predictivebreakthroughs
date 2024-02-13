7fad1f52410f055d19a055deeccc0e9318ffacb4
Matthew Davis <matt.davis@ascend-tech.us>
2024-02-12 19:03:29 -0500

    add better parameter help

--- mclCmd/src/main/java/gov/nih/opa/mcl/MCLRun.java
+++ mclCmd/src/main/java/gov/nih/opa/mcl/MCLRun.java
@@ -9,7 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "run", description = "The MCL algorithm is short for the Markov Cluster Algorithm, a fast and scalable unsupervised cluster algorithm for graphs (also known as networks) based on simulation of (stochastic) flow in graphs. More: https://micans.org/mcl/")
+@CommandLine.Command(name = "run", description = "Run the Markov Cluster Algorithm (MCL) or Regularized Markov Clustering Algorithm (RMCL).  MCL is fast and scalable unsupervised cluster algorithm for graphs based on simulation of stochastic flow in graphs. More on MCL here: https://micans.org/mcl/ and R-MCL here: https://cs.gmu.edu/~carlotta/SDM10Forum/posters/satuluri.pdf")
 public class MCLRun implements Callable<Integer>, MCLParameters {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MCLRun.class);
@@ -18,22 +18,22 @@ public class MCLRun implements Callable<Integer>, MCLParameters {
 	private MCL mcl;
 
 	@CommandLine.Option(names = {
-			"--inflation" }, description = "The inflation parameter to set", defaultValue = "2.0f", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+			"--inflation" }, description = "Higher inflation values result in more granular clusters, values closer to 1 (i.e. 1.2) will yield the larger clusters.  Recommended values between 1.1 and 8", defaultValue = "2.0f", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
 	public Float inflation = 2.0f;
 
-	@CommandLine.Option(names = "--loop", description = "Self loop ratio.", defaultValue = "0.001f", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+	@CommandLine.Option(names = "--loop", description = "Self loop weight", defaultValue = "0.001f", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
 	public Float loop = 0.001f;
 
-	@CommandLine.Option(names = "--pruningThreshold", description = "Minimum probability.", defaultValue = "0.0005f", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+	@CommandLine.Option(names = "--pruningThreshold", description = "Minimum weight of an edge before pruning after an iteration.  Setting this higher can speed up runtime at risk of causing overspliting", defaultValue = "0.0005f", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
 	public Float pruningThreshold = 0.0005f;
 
-	@CommandLine.Option(names = "--maxIterations", description = "Maximum iterations.", defaultValue = "250", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+	@CommandLine.Option(names = "--maxIterations", description = "Maximum iterations of the MCL algorithm", defaultValue = "250", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
 	public Integer maxIterations = 250;
 
-	@CommandLine.Option(names = "--shiftThreshold", description = "Shift threshold.", defaultValue = "0.0001f", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+	@CommandLine.Option(names = "--shiftThreshold", description = "Sets the minimum matrix value shift before terminating before max iterations", defaultValue = "0.0001f", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
 	public Float shiftThreshold = 0.0001f;
 
-	@CommandLine.Option(names = "--regularized", description = "Use R-MCL.", defaultValue = "true", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
+	@CommandLine.Option(names = "--regularized", description = "Use R-MCL instead of MCL which helps prevent over-splitting by regularizing against the input matrix", defaultValue = "true", showDefaultValue = CommandLine.Help.Visibility.ALWAYS)
 	public Boolean regularized = true;
 
 	@CommandLine.Option(names = {
